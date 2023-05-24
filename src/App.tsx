fed6a490970ad94365329a8ebfb610e42b49f2df
nils0n <nilson_jose.v@hotmail.com>
2023-05-23 21:08:49 -0300

    feat: add route to login

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-import { BrowserRouter } from "react-router-dom"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Header from "./components/Header"
+import Login from "./pages/Login"
 
 function App() {
 	return (
 		<BrowserRouter>
 			<Header />
+			<Routes>
+				<Route path="/login" element={<Login />} />
+			</Routes>
 		</BrowserRouter>
 
 	)
