cbf7a0581b7c56007a04f3ce4941a545c5ead5cd
nils0n <nilson_jose.v@hotmail.com>
2023-05-25 01:15:33 -0300

    feat: add graph to gols by minute

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,10 @@
 import Select from 'react-select';
+import { BarChart, CartesianGrid, XAxis, YAxis, Tooltip, Legend, Bar } from 'recharts';
+
 import CustomTable from '../../components/CustomTable';
 import LineUps from '../../components/LineUps';
+import Colors from '../../theme/theme.colors';
+
 import { ContainerPlayers, ContainerSelects, Flag, HomeContainer, HomeContent, ItemSelect } from './styles';
 
 const options = [
@@ -19,6 +23,24 @@ const players = [
 	{ name: 'Jon Joe', age: 23, nationality: 'Brasil' },
 	{ name: 'Jon Joe', age: 23, nationality: 'Brasil' },
 ];
+
+const data = [
+	{
+		"name": "0-15",
+		"total gols": 4,
+		"porcentagem": "6.06%"
+	},
+	{
+		"name": "16-30",
+		"total gols": 17,
+		"porcentagem": "25.76%"
+	},
+	{
+		"name": "31-45",
+		"total gols": 11,
+		"porcentagem": "16.67%"
+	}
+]
 
 function Home() {
 	return (
@@ -57,6 +79,15 @@ function Home() {
 					<LineUps nameImage='5-3-2' />
 				</ContainerPlayers>
 
+
+				<BarChart width={540} height={250} data={data}>
+					<CartesianGrid strokeDasharray="3 3" />
+					<XAxis dataKey="name" />
+					<YAxis />
+					<Tooltip />
+					<Legend />
+					<Bar dataKey="total gols" fill={Colors.background.blue} />
+				</BarChart>
 			</HomeContent>
 		</HomeContainer>
 	)
