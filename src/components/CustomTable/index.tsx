19cbc3f808d54165065af3087c997d9cf54ad193
nils0n <nilson_jose.v@hotmail.com>
2023-05-26 23:32:45 -0300

    fix: remove unused div

--- src/components/CustomTable/index.tsx
+++ src/components/CustomTable/index.tsx
@@ -15,32 +15,30 @@ type CustomTableProps = {
 function CustomTable({ data }: CustomTableProps) {
 	return (
 		<DivWrapper>
-			<div>
-				<TableCustom>
-					<TheadCustom>
-						<tr>
-							<th>Foto</th>
-							<th>Nome</th>
-							<th>Idade</th>
-							<th>Nacionalidade</th>
-						</tr>
-					</TheadCustom>
-					<TbodyCustom>
-						{data && data.length > 0 && (
-							data?.map((player, key) => (
-								<tr key={key}>
-									<td>
-										<img src={player.photo} alt="Foto jogador" style={{ width: '3rem', height: '3rem', borderRadius: '50%' }} />
-									</td>
-									<td>{player.name}</td>
-									<td>{player.age}</td>
-									<td>{player.nationality}</td>
-								</tr>
-							))
-						)}
-					</TbodyCustom>
-				</TableCustom>
-			</div>
+			<TableCustom>
+				<TheadCustom>
+					<tr>
+						<th>Foto</th>
+						<th>Nome</th>
+						<th>Idade</th>
+						<th>Nacionalidade</th>
+					</tr>
+				</TheadCustom>
+				<TbodyCustom>
+					{data && data.length > 0 && (
+						data?.map((player, key) => (
+							<tr key={key}>
+								<td>
+									<img src={player.photo} alt="Foto jogador" style={{ width: '3rem', height: '3rem', borderRadius: '50%' }} />
+								</td>
+								<td>{player.name}</td>
+								<td>{player.age}</td>
+								<td>{player.nationality}</td>
+							</tr>
+						))
+					)}
+				</TbodyCustom>
+			</TableCustom>
 		</DivWrapper>
 	)
 }
